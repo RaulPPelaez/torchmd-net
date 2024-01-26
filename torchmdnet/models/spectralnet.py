f498683de4d4adb769cf8d7c11a5b8aa21df1d4e
RaulPPealez <raulppelaez@gmail.com>
2024-01-26 10:22:43 +0100

    Add spectralnet

---
+++ torchmdnet/models/spectralnet.py
@@ -0,0 +1,9 @@
+import torch
+from typing import Optional, Tuple
+from torch import Tensor, nn
+from torchmdnet.models.utils import (
+    CosineCutoff,
+    OptimizedDistance,
+    rbf_class_mapping,
+    act_class_mapping,
+)
